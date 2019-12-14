7cc62c202dcf2f22af1eecdc552bda5ed85bd333
Catherine Smiley <catherinesmiley@gmail.com>
2019-12-14 15:56:17 +0000

    Automatically backed up by Learn

--- times.rb
+++ times.rb
@@ -1,13 +1,3 @@
-# def using_times
-# 	#your code here
-# end
-
-jewels_in_bag = 100
-
-3.times do
-  puts "Hiding 10 stolen jewels."
-  jewels_in_bag = jewels_in_bag - 10
-  puts "Now there are only #{jewels_in_bag} jewels left to hide!"
+def using_times
+	#your code here
 end
-
-puts "We have #{jewels_in_bag} jewels still to hide!"
